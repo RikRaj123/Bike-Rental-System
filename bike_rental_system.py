271c9099587c813d9eca951150e49cd2161de73d
RikRaj123 <92203867+RikRaj123@users.noreply.github.com>
2023-07-18 12:57:55 +0530

    Add files via upload

---
+++ bike_rental_system.py
@@ -0,0 +1,10 @@
+def bill(rented_bikes):
+    
+def bikedata():
+def availablity():
+    if(bike)
+def update_bikedata():
+
+def plans():
+    Rentalplan=int(input("ENTER 0 for Rental Plan:hourly/daily/weekly\nENTER 0 for Family Plan:30%\\discount on any 3 or more bikes rentals"))
+    return Rentalplan
