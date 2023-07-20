2edc1712f02faf5876d721a3f3682f41a4ade7f9
RikRaj123 <92203867+RikRaj123@users.noreply.github.com>
2023-07-20 16:12:44 +0530

    Update bike_rental_system.py

--- bike_rental_system.py
+++ bike_rental_system.py
@@ -1,10 +1,9 @@
-def bill(rented_bikes):
-    
-def bikedata():
-def availablity():
-    if(bike)
-def update_bikedata():
 
-def plans():
-    Rentalplan=int(input("ENTER 0 for Rental Plan:hourly/daily/weekly\nENTER 0 for Family Plan:30%\\discount on any 3 or more bikes rentals"))
-    return Rentalplan
+import pandas as pd
+import numpy as np
+bike_name=["Kawasaki Ninja 3000","Honda SB Shine","Royal Enfield","Activa 6G"]
+bike_milage=[10,100,20,60]
+bike_stock=[1,4,2,5]
+bikedata={"bike_name":bike_name,"bike_milage":bike_milage,"bike_stock":bike_stock}
+df = pd.DataFrame(bikedata)
+print(df)
