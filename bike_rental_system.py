a98644f958dffa954de79e4d3100889558353ce4
RikRaj123 <92203867+RikRaj123@users.noreply.github.com>
2023-09-03 21:45:01 +0530

    Delete bike_rental_system.py

--- bike_rental_system.py
+++
@@ -1,9 +0,0 @@
-
-import pandas as pd
-import numpy as np
-bike_name=["Kawasaki Ninja 3000","Honda SB Shine","Royal Enfield","Activa 6G"]
-bike_milage=[10,100,20,60]
-bike_stock=[1,4,2,5]
-bikedata={"bike_name":bike_name,"bike_milage":bike_milage,"bike_stock":bike_stock}
-df = pd.DataFrame(bikedata)
-print(df)
